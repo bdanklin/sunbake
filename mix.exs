420a073a4ed20d13a3661470143aa151c8d2fccc
Benjamin Danklin <mark@marks-engineering.com>
2021-09-04 21:49:44 +1000

    bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Sunbake.MixProject do
   def project do
     [
       app: :sunbake,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       description: description(),
